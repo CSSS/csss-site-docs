b9eb70d6b2a855335367dbc7ae08de8312520777
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-28 01:02:22 -0700

    dev: add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,29 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'CSSS Docs',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/CSSS/csss-site-docs'
+        }
+      ],
+      sidebar: [
+        {
+          label: 'General',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Contributing Documentation', slug: 'guides/docs-guide' }
+          ]
+        },
+        {
+          label: 'Event Guides',
+          items: [{ autogenerate: { directory: 'reference' } }]
+        }
+      ]
+    })
+  ]
 });
